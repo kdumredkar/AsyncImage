c88fa250ecaa524ebd3f1a1e7d240692ef164b68
KHIRAJ UMREDKAR <kdumredkar@gmail.com>
2023-12-19 23:06:49 +0530

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Khiraj Umredkar on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
